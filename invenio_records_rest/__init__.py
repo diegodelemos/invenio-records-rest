6c667d1f5fcdaf59c3951f3b34c985db86c01992
Dinos Kousidis <konstantinos.kousidis@cern.ch>
2017-11-24 14:09:29 +0100

    docs: usage
    Signed-off-by: Dinos Kousidis <konstantinos.kousidis@cern.ch>

--- invenio_records_rest/__init__.py
+++ invenio_records_rest/__init__.py
@@ -37,6 +37,93 @@ For further extending the REST API take a look at the guide in invenio-rest.
 Initialization
 --------------
 
+Create a Flask application:
+
+>>> import os
+>>> from flask import Flask
+>>> app = Flask('myapp')
+
+
+Instantiate all the dependencies
+
+>>> from invenio_db import InvenioDB
+>>> from invenio_rest import InvenioREST
+>>> from invenio_pidstore import InvenioPIDStore
+>>> from invenio_records import InvenioRecords
+>>> from invenio_records_rest import InvenioRecordsREST
+>>> from invenio_search import InvenioSearch
+>>> from invenio_indexer import InvenioIndexer
+>>> from invenio_records_rest.utils import PIDConverter
+
+>>> ext_db = InvenioDB(app)
+>>> InvenioREST(app)
+>>> InvenioPIDStore(app)
+>>> InvenioRecords(app)
+>>> search = InvenioSearch(app)
+>>> InvenioIndexer(app)
+>>> app.url_map.converters['pid'] = PIDConverter
+>>> InvenioRecordsREST(app)
+
+A basic configuration is needed to
+
+>>> index_name = 'testrecords-testrecord-v1.0.0'
+>>> app.config.update(
+...     INDEXER_DEFAULT_INDEX=index_name,
+...     INDEXER_DEFAULT_DOC_TYPE='testrecord-v1.0.0',
+...     SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI',
+...                                       'sqlite:///app.db'),
+...     RECORDS_REST_DEFAULT_READ_PERMISSION_FACTORY=None,
+...     RECORDS_REST_DEFAULT_CREATE_PERMISSION_FACTORY=None,
+...     RECORDS_REST_DEFAULT_DELETE_PERMISSION_FACTORY=None,
+...     RECORDS_REST_DEFAULT_UPDATE_PERMISSION_FACTORY=None,
+... )
+>>> app.config['RECORDS_REST_ENDPOINTS']['recid']['search_index'] = index_name
+
+For the following examples we will need an application context so let's
+push one:
+
+>>> app.app_context().push()
+
+Also, for the examples to work we need to create the database and tables:
+
+>>> from invenio_db import db
+>>> db.create_all()
+
+and finally for enabling indexing with Elasticsearch we need to create the
+indices and put the templates:
+
+>>> indices = list(search.create(ignore=[400]))
+>>> templates = search.put_templates(ignore=[400])
+
+The server is now ready to receive requests so we can start it with:
+
+>>> app.run()
+
+By default it uses port 5000.
+
+Let's create the first record. Open a new terminal and run:
+
+$ export DATA='{"title": "Test Record"}'
+$ curl -L -H 'Content-Type:application/json' -d $DATA -XPOST localhost:5000/records
+
+Adding the -L flag will follow the redirects and you will see in the output the
+created record.
+
+{
+  "created": "2017-11-24T10:52:42.761135+00:00",
+  "id": 1,
+  "links": {
+    "self": "http://localhost:5000/records/3"
+  },
+  "metadata": {
+    "control_number": "1",
+    "title": "Test Record"
+  },
+  "updated": "2017-11-24T10:52:42.761161+00:00"
+}
+
+To get the record you
+curl -XDELETE http://localhost:5000/records/3
 
 """
 
